db0d6c9e6eda00052432f00a233cfb3241723ff2
Chin Yeung Li <tli@nexb.com>
2020-10-13 08:33:51 +0800

    #1243
     * Remove the oelint_adv library
     * Upgrade oeline_parser's library
    
    
    Signed-off-by: Chin Yeung Li <tli@nexb.com>

--- src/packagedcode/bitbake.py
+++ src/packagedcode/bitbake.py
@@ -26,7 +26,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import io
 import logging
 
 import attr
@@ -46,8 +45,6 @@ if TRACE:
     logging.basicConfig(stream=sys.stdout)
     logger.setLevel(logging.DEBUG)
 
-# TODO: Override get_package_resource so it returns the Resource that the ABOUT file is describing
-
 @attr.s()
 class BitbakePackage(models.Package):
     metafiles = ('*.bb',)
@@ -59,8 +56,6 @@ class BitbakePackage(models.Package):
 
     @classmethod
     def get_package_root(cls, manifest_resource, codebase):
-        """
-        """
         return manifest_resource.parent(codebase)
 
 
@@ -131,6 +126,7 @@ def build_package(package_dict):
         download_url = package_dict['PREMIRRORS']
     #The item.VarName for SRC_URI[*] from the parser are SRC_URI
     #Therefore, I cannot differentiate md5,sha1, or src file location reference
+    # Entered an issue ticket: https://github.com/priv-kweihmann/oelint-parser/issues/3
     """
     if 'SRC_URI[sha1sum]' in package_dict:
         sha1 = package_dict['SRC_URI[sha1sum]']
@@ -148,7 +144,7 @@ def build_package(package_dict):
             dependencies += '\n' + package_dict['DEPENDS']
         else:
             dependencies = package_dict['DEPENDS']
-    # There are some RDEPENDS field such as "RDEPENDS_${PN}" which I need to
+    # There are some RDEPENDS_* fields such as "RDEPENDS_${PN}" which I need to
     # check with the substring
     for d in package_dict:
         if 'RDEPENDS' in d:
